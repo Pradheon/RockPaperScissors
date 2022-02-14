9542bf85a6414cb696faac9c1b8d94434ec48ddf
Joshan Rai <pradheon@gmail.com>
2022-02-14 13:46:55 -0800

    Initial Commit

---
+++ JoshanRai-RockPaperScissors/JoshanRai_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_RockPaperScissorsApp.swift
+//  JoshanRai-RockPaperScissors
+//
+//  Created by Joshan Rai on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
